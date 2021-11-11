d60f241edb91ed578770a0104475b2b99deda298
sergio666xd <sergiomunoz06062006@gmail.com>
2021-11-10 19:50:39 -0500

    Transición e inicio de settings

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -1,19 +1,60 @@
+$(function () {
+    $("nav").delegate("button", "click", function() {
+            window.location.hash = $(this).attr("id");
+            return false;
+    });
+});
 function aboutMe() {
+    $('.main').removeClass('normal');
+    $('#content').removeClass('normalc');
+    $('.main').addClass('wait');
+    $('#content').addClass('waitc');
     $('.main__content').load('./pages/about-me.html');
-    $('#aboutMe').addClass('active');
-    $('#habilities').removeClass('active');
-    $('#projects').removeClass('active');
+    setTimeout(function () {
+        $('#aboutMe').addClass('active');
+        $('#habilities').removeClass('active');
+        $('#projects').removeClass('active');
+    }, 100);
+    setTimeout(function () {
+        $('.main').removeClass('wait');
+        $('.main').addClass('normal');
+        $('#content').removeClass('waitc');
+        $('#content').addClass('normalc');
+    }, 500);
 }
-
 function habilities() {
+    $('.main').removeClass('normal');
+    $('#content').removeClass('normalc');
+    $('.main').addClass('wait');
+    $('#content').addClass('waitc');
     $('.main__content').load('./pages/habilities.html');
-    $('#aboutMe').removeClass('active');
-    $('#habilities').addClass('active');
-    $('#projects').removeClass('active');
+    setTimeout(function () {
+        $('#aboutMe').removeClass('active');
+        $('#habilities').addClass('active');
+        $('#projects').removeClass('active');
+    }, 100);
+    setTimeout(function () {
+        $('.main').removeClass('wait');
+        $('.main').addClass('normal');
+        $('#content').removeClass('waitc');
+        $('#content').addClass('normalc');
+    }, 500);
 }
 function projects() {
+    $('.main').removeClass('normal');
+    $('#content').removeClass('normalc');
+    $('.main').addClass('wait');
+    $('#content').addClass('waitc');
     $('.main__content').load('./pages/projects.html');
-    $('#aboutMe').removeClass('active');
-    $('#habilities').removeClass('active');
-    $('#projects').addClass('active');
+    setTimeout(function () {
+        $('#aboutMe').removeClass('active');
+        $('#habilities').removeClass('active');
+        $('#projects').addClass('active');
+    }, 100);
+    setTimeout(function () {
+        $('.main').removeClass('wait');
+        $('.main').addClass('normal');
+        $('#content').removeClass('waitc');
+        $('#content').addClass('normalc');
+    }, 500);
 }

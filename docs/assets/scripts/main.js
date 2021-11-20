a2d113fb76aaf1ee02c7ccfa51049e555a29c125
sergio666xd <sergiomunoz06062006@gmail.com>
2021-11-19 19:34:16 -0500

    se cancela configuración, se añaden animaciones, se reemplazan funciones en main.js y se añade contenido al footer

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -1,60 +1,61 @@
+let settings = document.querySelector("#settings");
+let viewvar = document.querySelector("#cstt__settings");
+
 $(function () {
-    $("nav").delegate("button", "click", function() {
-            window.location.hash = $(this).attr("id");
-            return false;
-    });
+	$("nav").delegate("button", "click", function() {
+			window.location.hash = $(this).attr("id");
+			return false;
+	});
+	settings.addEventListener("click",() => {
+		viewvar.classList.toggle("cstt__settings--view");
+	});
 });
+
+let abMe = document.querySelector("#aboutMe");
+let habs = document.querySelector("#habilities");
+let proj = document.querySelector("#projects");
+let main = document.querySelector(".main");
+let cont = document.querySelector("#content");
+
 function aboutMe() {
-    $('.main').removeClass('normal');
-    $('#content').removeClass('normalc');
-    $('.main').addClass('wait');
-    $('#content').addClass('waitc');
-    $('.main__content').load('./pages/about-me.html');
-    setTimeout(function () {
-        $('#aboutMe').addClass('active');
-        $('#habilities').removeClass('active');
-        $('#projects').removeClass('active');
-    }, 100);
-    setTimeout(function () {
-        $('.main').removeClass('wait');
-        $('.main').addClass('normal');
-        $('#content').removeClass('waitc');
-        $('#content').addClass('normalc');
-    }, 500);
+	main.classList.add("wait");
+	cont.classList.add("waitc");
+	$('.main__content').load('./pages/about-me.html');
+	setTimeout(function () {
+		abMe.classList.add("header__button--active");
+		habs.classList.remove("header__button--active");
+		proj.classList.remove("header__button--active");
+	}, 100);
+	setTimeout(function () {
+		main.classList.remove("wait");
+		cont.classList.remove("waitc");
+	}, 500);
 }
 function habilities() {
-    $('.main').removeClass('normal');
-    $('#content').removeClass('normalc');
-    $('.main').addClass('wait');
-    $('#content').addClass('waitc');
-    $('.main__content').load('./pages/habilities.html');
-    setTimeout(function () {
-        $('#aboutMe').removeClass('active');
-        $('#habilities').addClass('active');
-        $('#projects').removeClass('active');
-    }, 100);
-    setTimeout(function () {
-        $('.main').removeClass('wait');
-        $('.main').addClass('normal');
-        $('#content').removeClass('waitc');
-        $('#content').addClass('normalc');
-    }, 500);
+	main.classList.add("wait");
+	cont.classList.add("waitc");
+	$('.main__content').load('./pages/habilities.html');
+	setTimeout(function () {
+		abMe.classList.remove("header__button--active");
+		habs.classList.add("header__button--active");
+		proj.classList.remove("header__button--active");
+	}, 100);
+	setTimeout(function () {
+		main.classList.remove("wait");
+		cont.classList.remove("waitc");
+	}, 500);
 }
 function projects() {
-    $('.main').removeClass('normal');
-    $('#content').removeClass('normalc');
-    $('.main').addClass('wait');
-    $('#content').addClass('waitc');
-    $('.main__content').load('./pages/projects.html');
-    setTimeout(function () {
-        $('#aboutMe').removeClass('active');
-        $('#habilities').removeClass('active');
-        $('#projects').addClass('active');
-    }, 100);
-    setTimeout(function () {
-        $('.main').removeClass('wait');
-        $('.main').addClass('normal');
-        $('#content').removeClass('waitc');
-        $('#content').addClass('normalc');
-    }, 500);
+	main.classList.add("wait");
+	cont.classList.add("waitc");
+	$('.main__content').load('./pages/projects.html');
+	setTimeout(function () {
+		abMe.classList.remove("header__button--active");
+		habs.classList.remove("header__button--active");
+		proj.classList.add("header__button--active");
+	}, 100);
+	setTimeout(function () {
+		main.classList.remove("wait");
+		cont.classList.remove("waitc");
+	}, 500);
 } 
